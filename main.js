54fbe89284a08694e96c57302e9a8592c8764144
Chaphasilor <ppp.u@web.de>
2023-06-30 15:55:16 +0200

    added preview, finished input forms

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import './style.css'
 import { html, watch, reactive } from '@arrow-js/core'
 
-import { generateTextCode } from './src/printer-code'
+import * as codes from './src/printer-code'
 import { uploadFile } from './src/api'
 
 import javascriptLogo from './javascript.svg'
@@ -11,41 +11,17 @@ const tabs = {
   PCB_QR_CODES: `PCB QR Codes`,
   SIMPLE_TEXT: `Simple Text`,
   QR_CODE: `QR Code`,
-  DUMMY1: `Tab 4`,
-  DUMMY2: `Tab 5`,
-  DUMMY3: `Tab 6`,
 }
 
 const state = reactive({
   nav: tabs.PCB_QR_CODES,
-  localStates: {
-    // [tabs.PCB_QR_CODES]: {
-    //   startId: 0,
-    //   endId: 0,
-    //   amount: 0,
-    //   includePrefix: false,
-    //   prefixText: ``,
-    // },
-    // [tabs.SIMPLE_TEXT]: {
-    //   text: ``,
-    // },
-    // [tabs.QR_CODE]: {
-    //   text: ``,
-    // },
-    // [tabs.DUMMY1]: {
-    // },
-    // [tabs.DUMMY2]: {
-    // },
-    // [tabs.DUMMY3]: {
-    // },
-  },
   tabs: {},
 })
 
 const header = html`
   <header class="flex flex-col gap-6 p-6 md:rounded-br-[4rem] md:max-w-[75vw] lg:max-w-[60vw] bg-background-blue mb-10">
     <h1 class="font-semibold text-3xl text-dark-blue">EEL Small Label Printer</h1>
-    <p class="">Used for printing QR codes that identify PCBs within the AOI</p>
+    <p class="">Used for i.e. printing QR codes that identify PCBs within the AOI</p>
   </header>
   `
 
@@ -62,11 +38,14 @@ const nav = html`
   </nav>
 `
 
-function buildPrintButton(handler) {
+function buildPrintButton() {
   return html`
     <button
       class="pl-3 hover:bg-blue bg-light-blue pr-4 active:bg-bluer text-text-blue py-2 rounded-lg justify-center items-center gap-2 flex"
-      @click="${() => handler()}"
+      @click="${() => {
+        console.log(`print job:`, state.tabs[state.nav].preview)
+        uploadFile(state.tabs[state.nav].preview)
+      }}"
     >
       <svg
         class="stroke-[1.5px] w-6 h-6"
@@ -84,17 +63,18 @@ function buildPrintButton(handler) {
 }
 
 state.tabs[tabs.PCB_QR_CODES] = buildConfig(reactive({
-    startId: 0,
-    endId: 0,
-    amount: 0,
+    startId: 1,
+    endId: 1,
+    amount: 1,
     includePrefix: false,
     prefixText: ``,
-  }), 
+  }),
+  (localState) => ``,
   (localState) => html`
     <div class="bg-background-blue px-12 py-8 bg-slate-100 rounded-2xl flex-col justify-start items-start gap-8 inline-flex">
       <div class="flex flex-row items-center gap-8 w-full">
         <div class="p-0 grid justify-center items-center gap-8 w-24 font-medium aspect-square bg-white rounded-xl">
-          <img class="p-2 bg-white rounded-2xl flex-col justify-center items-center gap-2 inline-flex" src="https://via.placeholder.com/73x72" />
+          <img class="p-2 bg-white rounded-2xl flex-col justify-center items-center gap-2 inline-flex" src="https://via.placeholder.com/72x72" />
         </div>
         <div class="grow shrink basis-0 ">Create a batch of QR codes containing continuous IDs, with an optional label below the QR code</div>
       </div>
@@ -103,15 +83,15 @@ state.tabs[tabs.PCB_QR_CODES] = buildConfig(reactive({
           <div class="text-xs">Start ID</div>
           <input type="number" placeholder="15" class="w-20 text-opacity-60 px-4 py-2 bg-white rounded-lg border border-black justify-start items-start gap-2 inline-flex" value="${() => localState.startId}" @input="${e => {
             localState.startId = e.target.valueAsNumber
-            localState.amount = localState.endId - localState.startId
+            localState.amount = localState.endId - localState.startId + 1
           }}" />
         </div>
         <div class="p-0 justify-start items-center gap-4 inline-flex">
           <div class="p-0 flex-col justify-start items-start gap-2 inline-flex">
-            <div class="text-xs">End ID</div>
+            <div class="text-xs">End ID (inclusive)</div>
             <input type="number" placeholder="15" class="w-20 text-opacity-60 px-4 py-2 bg-white rounded-lg border border-black justify-start items-start gap-2 inline-flex" value="${() => localState.endId}" @input="${e => {
               localState.endId = e.target.valueAsNumber
-              localState.amount = localState.endId - localState.startId
+              localState.amount = localState.endId - localState.startId + 1
             }}" />
           </div>
           <div class="text-centertext-xs font-semibold">or</div>
@@ -119,24 +99,26 @@ state.tabs[tabs.PCB_QR_CODES] = buildConfig(reactive({
             <div class="text-xs">Amount</div>
             <input type="number" placeholder="15" class="w-20 text-opacity-60 px-4 py-2 bg-white rounded-lg border border-black justify-start items-start gap-2 inline-flex" value="${() => localState.amount}" @input="${e => {
               localState.amount = e.target.valueAsNumber
-              localState.endId = localState.startId + localState.amount
+              localState.endId = localState.startId + localState.amount - 1
             }}" />
           </div>
         </div>
-        <div class="pl-2 pr-4 pt-2 pb-4 rounded-lg border border-black flex-col justify-start items-start gap-3 flex">
+        <div class="${() => `pl-2 pr-4 pt-2 pb-4 rounded-lg border border-black flex-col justify-start items-start gap-3 flex ${!localState.includeLabel && `border-gray-400`}`}">
           <div class="p-0 justify-start items-center gap-2 inline-flex">
-            <input type="checkbox" class="w-4 h-4" />
-            <div class="">Include Prefix</div>
+            <input type="checkbox" class="w-4 h-4" id="${`${tabs.PCB_QR_CODES}-label`}" checked="${() => localState.includePrefix}" @input="${(e) => {
+              localState.includePrefix = e.target.checked
+            }}" />
+            <label class="" for="${`${tabs.PCB_QR_CODES}-label`}">Include Prefix</label>
           </div>
-          <div class="pl-8 flex-col justify-start items-start gap-2 flex">
+          <div class="${() => `pl-8 flex-col justify-start items-start gap-2 flex ${!localState.includePrefix && `opacity-50`}`}">
             <div class="text-xs">Prefix Text</div>
-            <input type="text" placeholder="SN: " class="w-48 text-opacity-60 px-4 py-2 bg-white rounded-lg border border-black justify-start items-start gap-2 inline-flex" />
+            <input type="text" placeholder="SN: " class="w-48 text-opacity-60 px-4 py-2 bg-white rounded-lg border border-black disabled:cursor-not-allowed justify-start items-start gap-2 inline-flex" value="${() => localState.prefixText}" disabled="${() => !localState.includePrefix}" @input="${(e) => {
+              localState.prefixText = e.target.value
+            }}" />
           </div>
         </div>
       </div>
-      ${buildPrintButton(() => {
-        console.log(`localState:`, localState)
-      })}
+      ${buildPrintButton()}
     </div>
   `
 )
@@ -145,7 +127,9 @@ state.tabs[tabs.SIMPLE_TEXT] = buildConfig(reactive({
   text: `some\nsample\ntext`,
   manualFontSize: false,
   fontSize: 7,
-}), 
+  preview: ``,
+}),
+(localState) => codes.generateTextCode(localState.text, localState.manualFontSize ? localState.fontSize : -1),
 (localState) => html`
   <div class="bg-background-blue px-12 py-8 bg-slate-100 rounded-2xl flex-col justify-start items-start gap-8 inline-flex">
     <div class="flex flex-row items-center gap-8 w-full">
@@ -164,32 +148,82 @@ state.tabs[tabs.SIMPLE_TEXT] = buildConfig(reactive({
         }}"
       >${localState.text}</textarea>
     </div>
-    <div class="pl-2 pr-4 pt-2 pb-4 rounded-lg border border-black flex-col justify-start items-start gap-3 flex">
+    <div class="${() => `pl-2 pr-4 pt-2 pb-4 rounded-lg border border-black flex-col justify-start items-start gap-3 flex ${!localState.manualFontSize && `border-gray-400`}`}">
       <div class="p-0 justify-start items-center gap-2 inline-flex">
-        <input type="checkbox" class="w-4 h-4" checked="${() => localState.manualFontSize}" @input="${e => {
+        <input type="checkbox" id="${`${tabs.SIMPLE_TEXT}-label`}" class="w-4 h-4" checked="${() => localState.manualFontSize}" @input="${e => {
           console.log(`e:`, e)
           localState.manualFontSize = e.target.checked
         }}" />
-        <div class="">Manually Set Font Size</div>
+        <label class="" for="${`${tabs.SIMPLE_TEXT}-label`}">Manually Set Font Size</label>
       </div>
-      <div class="pl-8 flex-col justify-start items-start gap-2 flex">
+      <div class="${() => `pl-8 flex-col justify-start items-start gap-2 flex ${!localState.manualFontSize && `opacity-50`}`}">
         <div class="text-xs">Font Size</div>
         <input
-          class="w-20 text-opacity-60 px-4 py-2 bg-white rounded-lg border border-black justify-start items-start gap-2 inline-flex" 
+          class="w-20 text-opacity-60 px-4 py-2 bg-white rounded-lg border border-black disabled:cursor-not-allowed justify-start items-start gap-2 inline-flex" 
           type="number"
           placeholder="6"
           value="${() => localState.fontSize}"
+          disabled="${() => !localState.manualFontSize}"
           @input="${e => {
             localState.fontSize = e.target.valueAsNumber
           }}"
       />
       </div>
     </div>
-    ${buildPrintButton(() => {
-      console.log(`localState:`, localState)
-      uploadFile(generateTextCode(localState.text, localState.manualFontSize ? localState.fontSize : -1))
-      // console.log(generateTextCode(localState.text, localState.manualFontSize ? localState.fontSize : -1))
-    })}
+    ${buildPrintButton()}
+  </div>
+`
+)
+
+state.tabs[tabs.QR_CODE] = buildConfig(reactive({
+  text: ``,
+  amount: 1,
+  includeLabel: false,
+  labelText: ``,
+}),
+(localState) => codes.generateQrCode(localState.text, localState.includeLabel ? localState.labelText : null, localState.amount),
+(localState) => html`
+  <div class="bg-background-blue px-12 py-8 bg-slate-100 rounded-2xl flex-col justify-start items-start gap-8 inline-flex">
+    <div class="flex flex-row items-center gap-8 w-full">
+      <div class="p-0 grid justify-center items-center gap-8 w-24 font-medium aspect-square bg-white rounded-xl">
+        <img class="p-2 bg-white rounded-2xl flex-col justify-center items-center gap-2 inline-flex" src="https://via.placeholder.com/72x72" />
+      </div>
+      <div class="grow shrink basis-0 ">Create QR codes containing custom text, with an optional label below the QR code</div>
+    </div>
+    <div class="p-0 flex-col w-full justify-start items-start gap-4 flex">
+      <div class="p-0 flex-col w-full justify-start items-start gap-2 flex">
+        <div class="text-xs">Text</div>
+        <textarea
+          class="w-full min-h-[6rem] overflow-hidden text-opacity-60 resize px-4 py-2 bg-white rounded-lg border border-black"
+          placeholder="Content of QR Code"
+          @input="${e => {
+            localState.text = e.target.value
+          }}"
+        >${localState.text}</textarea>
+      </div>
+      <div class="p-0 flex-col justify-start items-start gap-2 inline-flex">
+        <div class="text-xs">Amount</div>
+        <input type="number" placeholder="15" class="w-20 text-opacity-60 px-4 py-2 bg-white rounded-lg border border-black justify-start items-start gap-2 inline-flex" value="${() => localState.amount}" @input="${e => {
+          localState.amount = e.target.valueAsNumber
+          localState.endId = localState.startId + localState.amount
+        }}" />
+      </div>
+      <div class="${() => `pl-2 pr-4 pt-2 pb-4 rounded-lg border border-black flex-col justify-start items-start gap-3 flex ${!localState.includeLabel && `border-gray-400`}`}">
+        <div class="p-0 justify-start items-center gap-2 inline-flex">
+          <input type="checkbox" class="w-4 h-4" id="${`${tabs.QR_CODE}-label`}" checked="${() => localState.includeLabel}" @input="${(e) => {
+            localState.includeLabel = e.target.checked
+          }}" />
+          <label class="" for="${`${tabs.QR_CODE}-label`}">Include Label</label>
+        </div>
+        <div class="${() => `pl-8 flex-col justify-start items-start gap-2 flex ${!localState.includeLabel && `opacity-50`}`}">
+          <div class="text-xs">Label Text</div>
+          <input type="text" placeholder="Label" class="w-48 text-opacity-60 px-4 py-2 bg-white rounded-lg disabled:cursor-not-allowed border border-black justify-start items-start gap-2 inline-flex" value="${() => localState.labelText}" disabled="${() => !localState.includeLabel}" @input="${(e) => {
+            localState.labelText = e.target.value
+          }}" />
+        </div>
+      </div>
+    </div>
+    ${buildPrintButton()}
   </div>
 `
 )
@@ -202,16 +236,35 @@ const config = html`
   ${() => state.tabs[state.nav]?.html}
 `
 
-function buildConfig(localState, body) {
+function buildConfig(localState, previewFunction, body) {
   return {
     state: localState,
-    html: body(localState)
+    preview: previewFunction(localState),
+    html: body(localState),
   }
 }
 
 const preview = html`
-  <div class="border border-border rounded-2xl overflow-y-auto">
-  </div>
+  <pre class="relative border border-border rounded-2xl overflow-y-auto py-2 px-3">
+${() => state.tabs[state.nav]?.preview}
+    <button
+      class="absolute bottom-3 right-3 pl-3 hover:bg-blue bg-light-blue pr-4 active:bg-bluer text-text-blue py-2 rounded-lg justify-center items-center gap-2 flex"
+      @click="${() => {
+        navigator.clipboard.writeText(state.tabs[state.nav].preview)
+      }}"
+    >
+      <svg
+        class="stroke-[1.5px] w-6 h-6"
+        viewBox="0 0 24 24" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <path d="M8 8m0 2a2 2 0 0 1 2 -2h8a2 2 0 0 1 2 2v8a2 2 0 0 1 -2 2h-8a2 2 0 0 1 -2 -2z"></path>
+        <path d="M16 8v-2a2 2 0 0 0 -2 -2h-8a2 2 0 0 0 -2 2v8a2 2 0 0 0 2 2h2"></path>
+      </svg>
+      <span class="font-sans">Copy To Clipboard</span>
+    </button>
+  </pre>
 `
 
 const main = html`
--- src/printer-code.js
+++ src/printer-code.js
@@ -1,3 +1,22 @@
+/** 
+ * calculate font size based on number of lines and maximum number of characters per line
+ * @param {string} text
+ */
+function getDynamicFontSize(text) {
+  
+  const lines = text.split(`\n`)
+  const maxLineLength = Math.max(...lines.map(line => line.length))
+  const maxFontSize = 10
+  const minFontSize = 3
+  const fontSizeForLine = maxFontSize / (lines.length/2)
+  const fontSizeForLength = maxFontSize / (maxLineLength/4)
+
+  let fontSize = Number(Math.min(maxFontSize, Math.max(minFontSize, Math.min(fontSizeForLine, fontSizeForLength))).toFixed(2))
+
+  return fontSize
+  
+}
+
 /**
  * 
  * sample code:
@@ -7,22 +26,15 @@
  * O R
  * T 2,6.5,0,5,pt10;10pF
  * A 1
- * @param {*} text 
- * @param {*} fontSize 
+ * @param {String} text 
+ * @param {Number} fontSize 
  * @returns 
  */
 export function generateTextCode(text, fontSize) {
 
   fontSize = fontSize || 7
   if (fontSize === -1) {
-    // calculate font size based on number of lines and maximum number of characters per line
-    const lines = text.split(`\n`)
-    const maxLineLength = Math.max(...lines.map(line => line.length))
-    const maxFontSize = 10
-    const minFontSize = 3
-    const fontSizeForLine = maxFontSize / (lines.length/2)
-    const fontSizeForLength = maxFontSize / (maxLineLength/4)
-    fontSize = Math.min(maxFontSize, Math.max(minFontSize, Math.min(fontSizeForLine, fontSizeForLength)))
+    fontSize = getDynamicFontSize(text)
   }
   console.log(`fontSize:`, fontSize)
   const lines = text.split(`\n`)
@@ -34,6 +46,34 @@ J
 S l1;0.0,0.0,10,13,10
 O R
 ${linesCode}
+A 1
+`
+}
+
+/**
+ * 
+ * sample code:
+ * 
+ * @param {String} text 
+ * @param {String?} label 
+ * @param {Number?} amount
+ * @returns 
+ */
+export function generateQrCode(text, label = null, amount = 1) {
+
+  let fontSize = getDynamicFontSize(text)
+  console.log(`fontSize:`, fontSize)
+
+  let content = label ? 
+    `B 1.9,0.8,0,QRCODE+EL1,0.3;${text}
+    T 1,9.5,0,5,pt4.8;${label}` :
+    `B 1.5,1.4,0,QRCODE+EL1,0.3;${text}`
+  
+  return `m m
+J
+S l1;0.0,0.0,10,13,10
+O R
+${content}
 A 1
 `
 }
